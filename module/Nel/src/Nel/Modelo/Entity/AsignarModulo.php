6462219041f6396629ef4522bcb55fef9054f61b
dayubadel <dayana_bailon@outlook.com>
2018-09-05 11:41:01 -0500

    Se agrego el catalogo AsignarModulo y el metodo FiltrarModulosPorUsuario

---
+++ module/Nel/src/Nel/Modelo/Entity/AsignarModulo.php
@@ -0,0 +1,59 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Nel\Modelo\Entity;
+
+use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\Adapter\Adapter;
+
+class AsignarModulo extends TableGateway
+{
+    public function __construct(Adapter $adapter = null, $databaseSchema = null, ResultSet $selectResultPrototype = null)
+    {
+        return parent::__construct('asignarmodulo', $adapter, $databaseSchema, $selectResultPrototype);
+    }
+    
+    
+    public function FiltrarModulosPorUsuario($idUsuario){
+        $resultado = $this->getAdapter()->query("CALL Sp_FiltrarModulosPorUsuario('{$idUsuario}')", Adapter::QUERY_MODE_EXECUTE)->toArray();
+        return $resultado;
+    }
+//    public function IngresarMisa($descripcionMisa,$fechaIngresoMisa,$estadoMisa){
+//        $resultado = $this->getAdapter()->query("CALL Sp_IngresarMisa('{$descripcionMisa}','{$fechaIngresoMisa}','{$estadoMisa}')", Adapter::QUERY_MODE_EXECUTE)->toArray();
+//        return $resultado;
+//    }
+//    public function FiltrarMisaPorDescripcion($descripcionMisa){
+//        $resultado = $this->getAdapter()->query("CALL Sp_FiltrarMisaPorDescripcion('{$descripcionMisa}')", Adapter::QUERY_MODE_EXECUTE)->toArray();
+//        return $resultado;
+//    }
+//    
+//    
+//    
+//    public function FiltrarPersona($idPersona){
+//        $resultado = $this->getAdapter()->query("CALL Sp_FiltrarPersona('{$idPersona}')", Adapter::QUERY_MODE_EXECUTE)->toArray();
+//        return $resultado;
+//    }
+//    
+//    public function FiltrarPersonaPorIdentificacion($identificacion){
+//        $resultado = $this->getAdapter()->query("CALL Sp_FiltrarPersonaPorIdentificacion('{$identificacion}')", Adapter::QUERY_MODE_EXECUTE)->toArray();
+//        return $resultado;
+//    }
+//    
+//
+//    
+//    public function EliminarLugarMisa($idLugarMisa){
+//        $resultado = $this->getAdapter()->query("CALL Sp_EliminarLugarMisa('{$idLugarMisa}')", Adapter::QUERY_MODE_EXECUTE)->toArray();
+//        return $resultado;
+//    }
+    
+    
+   
+
+
+
+}
